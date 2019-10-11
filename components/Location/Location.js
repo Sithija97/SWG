8ffdff815dd357e2df2cc16ff130cb3f0b61e9ca
Sithija Shehara <nsithijashehara@gmail.com>
2019-10-11 10:02:14 +0530

    updated location screen

--- components/Location/Location.js
+++ components/Location/Location.js
@@ -7,33 +7,43 @@ import {
   Text,
   StatusBar,
   TextInput,
-  Image
+  Image,
 } from 'react-native';
 
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import { Marker } from 'react-native-maps';  
 
 export default class LocationScreen extends React.Component {
   static navigationOptions = {
-    drawerIcon: ()=>(
-      <Image  source={require('./location.png')}
-      style={styles.image}
-      />
+    drawerIcon: () => (
+      <Image source={require('./location.png')} style={styles.image} />
     ),
-  }
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 6.8511,
-            longitude: 79.9212,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}></MapView>
+    return(
+      <View style={styles.MainContainer}>  
+  
+        <MapView  
+          style={styles.mapStyle}  
+          showsUserLocation={false}  
+          zoomEnabled={true}  
+          zoomControlEnabled={true}  
+          initialRegion={{  
+            latitude: 6.8511,   
+            longitude: 79.9212,  
+            latitudeDelta: 0.015,  
+            longitudeDelta: 0.0121,  
+          }}>  
+  
+          <Marker  
+            coordinate={{ latitude: 28.579660, longitude: 77.321110 }}  
+            title={"JavaTpoint"}  
+            description={"Java Training Institute"}  
+          />  
+        </MapView>  
+          
       </View>
-    );
+    ); 
   }
 }
 
@@ -48,16 +58,36 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  input: {
-    height: 40,
-    backgroundColor: '#ffffff',
-    marginBottom: 20,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-  },
   image: {
     width: 30,
-    height: 30
+    height: 30,
+  },
+  MainContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  mapStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
+/*
+    <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 6.8511,
+            longitude: 79.9212,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}></MapView>
+*/
